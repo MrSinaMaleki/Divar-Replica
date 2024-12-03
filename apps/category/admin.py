0d881348792faa347ed59df961f53689f63aa76d
mrsinamaleki <sina67631@gmail.com>
2024-12-03 16:15:22 +0330

    only god users can see the category and field models

--- apps/category/admin.py
+++ apps/category/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from apps.category.models import Category, Field
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Field)
+
+class CategoryAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        if request.user.role == 'god':
+            return super().get_model_perms(request)
+        return {}
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class FieldAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        if request.user.role == 'god':
+            return super().get_model_perms(request)
+        return {}
+
+admin.site.register(Field, FieldAdmin)
