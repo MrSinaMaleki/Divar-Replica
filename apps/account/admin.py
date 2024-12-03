63a8ecfbb701e46e51d2bb77e6d677861a9a2c58
mrsinamaleki <sina67631@gmail.com>
2024-12-03 22:54:03 +0330

    user admin pannel mofifications

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from apps.account.models import User
+
+
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'role', 'nationality', 'is_dad')
+    list_filter = ('is_dad', )
+
+admin.site.register(User, UserAdmin)
