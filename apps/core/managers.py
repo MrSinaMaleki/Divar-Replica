9714eedaf8f95774e4a125b4d007dbecce04a006
mrsinamaleki <sina67631@gmail.com>
2024-12-13 18:37:07 +0330

    more accurate use of managers a logical models

--- apps/core/managers.py
+++ apps/core/managers.py
@@ -1,27 +1,7 @@
 from django.db import models
 
 
-class LogicalMixin(models.Model):
-    is_active = models.BooleanField(default=True)
-    is_delete = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class ActiveNotDeletedBaseManager(models.Manager):
+    def get_queryset(self):
 
-    class Meta:
-        abstract = True
-
-    def activate(self):
-        self.is_active = True
-        self.save(update_fields=['is_active'])
-
-    def deactivate(self):
-        self.is_active = False
-        self.save(update_fields=['is_active'])
-
-    def make_delete(self):
-        self.is_delete = True
-        self.save(update_fields=['is_delete'])
-
-    def make_undelete(self):
-        self.is_delete = False
-        self.save(update_fields=['is_delete'])
+        return super().get_queryset().filter(is_active=True, is_delete=False)
