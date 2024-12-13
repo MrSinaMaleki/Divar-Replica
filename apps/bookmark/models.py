e6529c1a69ee96236bb8ae4ba46c2b94d99a95de
mrsinamaleki <sina67631@gmail.com>
2024-12-13 18:41:34 +0330

    removal of notes , addition of bookmark

--- apps/bookmark/models.py
+++ apps/bookmark/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from apps.account.models import User
+from apps.core.models import LogicalMixin
 from apps.post.models import Post
+from apps.core.managers import ActiveNotDeletedBaseManager
 
-class Bookmark(models.Model):
+
+class Bookmark(LogicalMixin):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_query_name="bookmarks", related_name='bookmarks')
-    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
-    notes = models.TextField(blank=True, null=True)
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE, related_query_name="posts",
+                                 related_name='posts')
 
+    objects = ActiveNotDeletedBaseManager()
 
     def __str__(self):
-        return f"Bookmark: {self.user} -> {self.posts}"
+        return f"{self.user} -> {self.products}"
+
+    # class Meta:
+    #     constraints = [
+    #         models.UniqueConstraint(fields=['user', 'products'], name='unique_favorite')
+    #     ]
